47b2125368748084b2fa6c2927f888620d1dd455
lJeskl <jesus.zuluaga@correounivalle.edu.co>
2021-05-10 20:10:12 -0500

    first commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/videobackground.mp4' autoPlay loop muted />
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Nuestro Menú   
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
